6770d655649c31440595fe26dc940c63c4f4ed4f
Rizqa Tsaqila Aulia Haq <40709260+rizqatsah@users.noreply.github.com>
2019-06-21 15:02:10 +0700

    Update pembelian.php

--- pembelian.php
+++ pembelian.php
@@ -1,17 +1,35 @@
 <?php 
   include 'db.php';
+  	if($_POST) {
+    # code...
+    
+    // $kode = 'exol';
+    
 
-    $kodekonser = $_GET['kodekonser'];
-    $query3 = "SELECT * FROM konser WHERE kodekonser='$kodekonser'";
-    $SQL = mysqli_query($connect, $query3);
-    $data = mysqli_fetch_array($SQL);
+    $id= $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $hp = $_POST['hp'];
+    $query2 = "INSERT INTO customer VALUES ('$id', '$email', '$name', '$hp')";
 
-    $query1 = "SELECT * FROM tiket WHERE kodekonser='$kodekonser'";
-    $SQL1 = mysqli_query($connect, $query1);
 
-  
+    // $tgl_beli = date("Y-m-d", strtotime($_POST['tgl_beli']));
+    // $tgl_antar = date("Y-m-d", strtotime($_POST['tgl_antar']));
+    $kodetiket = $_POST['kodetiket'];
+    $jumlah = $_POST['jumlah'];
 
- ?>
+    $query = "INSERT INTO pembelian VALUES ('', '$jumlah', '$id', '$kodetiket')";
+    $sql2 = mysqli_query($connect, $query2);
+    $sql1 = mysqli_query($connect, $query);
+
+    $query3 = "SELECT `customer`.`id`, `customer`.`email`, `customer`.`name`, `customer`.`hp`, `konser`.`namakonser`, `konser`.`tanggal`, `konser`.`waktu`, `konser`.`namatempat`, `tiket`.`namatiket`, `tiket`.`harga`, `pembelian`.`jumlah`, `pembelian`.`jumlah`*`tiket`.`harga` as 'total' , `pembelian`.`kodebeli` FROM `customer` JOIN `pembelian` ON `customer`.`id` = `pembelian`.`id` JOIN `tiket` ON `pembelian`.`kodetiket`=`tiket`.`kodetiket` JOIN `konser` ON `tiket`.`kodekonser`=`konser`.`kodekonser` WHERE `customer`.`id` = '$id'";
+    
+    $sql = mysqli_query($connect, $query3);
+    $data = mysqli_fetch_array($sql);
+
+  }
+  	
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +53,8 @@
   <!-- Custom styles for this template -->
   <link href="css/business-casual.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="style_pesan.css">
-  <script src="validasi.js"></script>
+	<script src="formvalidation.js"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <style>
     .caption{
       color: azure;
@@ -63,26 +82,31 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav mx-auto">
           <li class="nav-item active px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="index.html">Home
+            <a class="nav-link text-uppercase text-expanded" href="index.php">Concert
               <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="venue.php">Venue</a>
+          </li>
+          <li class="nav-item px-lg-4">
             <a class="nav-link text-uppercase text-expanded" href="about.html">About Us</a>
-          </li>
-          <li class="nav-item px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="concert.html" >Concert</a>
-          </li>
-          <li class="nav-item px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="venue.html">Venue</a>
           </li>
           
           <li class="nav-item px-lg-4">
             <!-- <a class="nav-link text-uppercase text-expanded" >Search</a> -->
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <form class="form-inline my-2 my-lg-0"  method="get" action="index.php">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search..." aria-label="Search" name="cari" >
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>          
+          </li>
+          <li class="nav-item px-lg-4">
+            <!-- <a class="nav-link text-uppercase text-expanded" >Search</a> -->
+            <a class="nav-link text-uppercase text-expanded" href="login.html">
+              <i class="large material-icons" >account_circle</i>
+              Admin Login     
+            </a>  
+             
           </li>
         </ul>
       </div>
@@ -91,13 +115,21 @@
 <div>
   <br><br><br>
 </div>
-<form method="post" action="datavalidation.php" name="myForm" onsubmit="return validateForm()">
+		  <div class="row">
+            <div class="col1">
+              <label>KODE BELI</label>
+            </div>
+            <div class="col2">
+              <label><?php echo $data['kodebeli']?></label>
+            </div>
+          </div>
+
           <div class="row">
             <div class="col1">
               <label>ID</label>
             </div>
             <div class="col2">
-              <input type="text" name="id" >
+              <label><?php echo $data['id']?></label>
             </div>
           </div>
           <div class="row">
@@ -105,7 +137,7 @@
               <label>E-Mail</label>
             </div>
             <div class="col2">
-              <input type="text" name="email" >
+              <label><?php echo $data['email']?></label>
             </div>
           </div>
           <div class="row">
@@ -113,7 +145,7 @@
               <label>Name</label>
             </div>
             <div class="col2">
-              <input type="text" name="name" >
+              <label><?php echo $data['name']?></label>
             </div>
           </div>
           <div class="row">
@@ -121,7 +153,7 @@
               <label>Contact</label>
             </div>
             <div class="col2">
-              <input type="text" name="hp" >
+              <label><?php echo $data['hp']?></label>
             </div>
           </div>
           
@@ -171,58 +203,15 @@
             </div>
           </div>
 
-          <div class="row">
-            <div class="col1">
-              <label>KATEGORI TIKET</label>
-            </div>
-            <div class="col2">
-              <label>
-              <table class="data" border="5" align="center" cellspacing="3"> 
-                  <tr> 
-                      <td>KATEGORI TIKET</td>
-                      <td>NAMA TIKET</td>
-                      <td>HARGA (IDR)</td>
-                  </tr>
-                  <?php
-                  include 'db.php';
-                   $query1 = "SELECT `tiket`.`namatiket`, `kategoritiket`.`namakategori`, `tiket`.`harga` FROM tiket JOIN kategoritiket ON `tiket`.`kodekategori`=`kategoritiket`.`kodekategori` WHERE `kodekonser`='$kodekonser'";
-                   $SQL1 = mysqli_query($connect, $query1);
-                      foreach ($SQL1 as $data) {
-                        echo "
-                          <tr>
-                            <td>$data[namakategori]</td>
-                            <td>$data[namatiket]</td>
-                            <td>$data[harga]</td>
-                          </tr>
-                        ";
-                      }     
-                  ?>
-              </table>
-            </label>
-            </div>
-          </div>
-<div>
-  <br><br><br>
-  <br><br><br>
-</div>
+          
+
           <div class="row">
             <div class="col1">
               <label>TIKET KONSER</label>
             </div>
             
             <div class="col2">
-              <select name="kodetiket" >
-                <option>PILIH TIKET</option>
-                  <?php 
-                    include 'db.php';
-                    $query3 = "SELECT * FROM tiket WHERE kodekonser='$kodekonser'";
-                    $sql3 = mysqli_query($connect, $query3);
-                    while ($row = mysqli_fetch_array($sql3))
-                    {
-                      echo "<option value='".$row['kodetiket']."'>'".$row['namatiket']."'</option>";
-                    }
-                  ?>        
-              </select>
+				<label><?php echo $data['namatiket']?></label>
             </div>
           </div>
 
@@ -231,23 +220,35 @@
               <label>Jumlah</label>
             </div>
             <div class="col2">
-              <input type="number" name="jumlah" >
+              <label><?php echo $data['jumlah']?></label>
             </div>
           </div>
+
           <div class="row">
             <div class="col1">
-              <label>PASTIKAN DATA YANG ANDA INPUTKAN BENAR</label>
+              <label>Total</label>
             </div>
             <div class="col2">
-              <p></p>
+              <label><?php echo $data['total']?></label>
             </div>
           </div>
-          <div>
-             <input type="submit">
-          </div>
-        </form>
+
+
 <div>
-  <br><br><br>
+  <br>
+</div>
+		  <div class="row">
+            <div class="col1">
+              <label>
+              <h5>SILAKAN TRANSFER PEMBAYARAN KE NOMOR REKENING DI BAWAH INI</h5>
+              <P>01234567890 BNI a.n Q'INs Entertaintment</P>
+          
+              <p>Batas Pembayaran 24 jam</p>
+            </label>
+            </div>
+          </div>
+<div>
+  <br><br><br><br><br><br>
 </div>
   <footer class="footer text-faded text-center py-5">
     <div class="container">
