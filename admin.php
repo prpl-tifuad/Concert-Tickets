cb9954e50eeb5d71f13ecaaed5fc578b4d24afa6
Rizqa Tsaqila Aulia Haq <40709260+rizqatsah@users.noreply.github.com>
2019-06-21 14:21:31 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,3 +1,36 @@
+<?php 
+  include 'db.php';
+  	if($_POST) {
+    # code...
+    
+    // $kode = 'exol';
+    
+
+    $id= $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $hp = $_POST['hp'];
+    $query2 = "INSERT INTO customer VALUES ('$id', '$email', '$name', '$hp')";
+
+
+    // $tgl_beli = date("Y-m-d", strtotime($_POST['tgl_beli']));
+    // $tgl_antar = date("Y-m-d", strtotime($_POST['tgl_antar']));
+    $kodetiket = $_POST['kodetiket'];
+    $jumlah = $_POST['jumlah'];
+
+    $query = "INSERT INTO pembelian VALUES ('', '$jumlah', '$id', '$kodetiket')";
+    $sql2 = mysqli_query($connect, $query2);
+    $sql1 = mysqli_query($connect, $query);
+
+    $query3 = "SELECT `customer`.`id`, `customer`.`email`, `customer`.`name`, `customer`.`hp`, `konser`.`namakonser`, `konser`.`tanggal`, `konser`.`waktu`, `konser`.`namatempat`, `tiket`.`namatiket`, `tiket`.`harga`, `pembelian`.`jumlah`, `pembelian`.`jumlah`*`tiket`.`harga` as 'total' , `pembelian`.`kodebeli` FROM `customer` JOIN `pembelian` ON `customer`.`id` = `pembelian`.`id` JOIN `tiket` ON `pembelian`.`kodetiket`=`tiket`.`kodetiket` JOIN `konser` ON `tiket`.`kodekonser`=`konser`.`kodekonser` WHERE `customer`.`id` = '$id'";
+    
+    $sql = mysqli_query($connect, $query3);
+    $data = mysqli_fetch_array($sql);
+
+  }
+  	
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,15 +42,15 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Dashboard</title>
+  <title>Q'INs Admin - Dashboard</title>
 
   <!-- Custom fonts for this template-->
   <link href="css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
+  <script src="functionadmin.js"></script>
 </head>
 
 <body id="page-top">
@@ -51,7 +84,7 @@
 
       <!-- Heading -->
       <div class="sidebar-heading">
-        Interface
+        INPUT DATA
       </div>
 
       <li class="nav-item">
@@ -61,16 +94,19 @@
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="inputconcert.php">
+        <a class="nav-link" href="inputkonser.php">
         <i class="large material-icons" >event</i>
           <span>Concert</span></a>
       </li>
     
       <li class="nav-item">
-        <a class="nav-link" >
+        <a class="nav-link" href="inputtiket.php">
         <i class="large material-icons" >note</i>
           <span>Ticket</span></a>
+        
       </li>
+
+      
 
     </ul>
     <!-- End of Sidebar -->
@@ -90,11 +126,11 @@
           </button>
 
           <!-- Topbar Search -->
-          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" name="searching" >
             <div class="input-group">
-              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+              <input type="search" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" name="cari">
               <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
+                <button class="btn btn-primary" onclick="searchAdmin()" >
                   <i class="large material-icons">search</i>
                 </button>
               </div>
@@ -104,7 +140,7 @@
           
           <div class="topbar-divider d-none d-sm-block"></div>
 
-          <a class="navbar-nav ml-auto" href="#" data-toggle="modal" data-target="#logoutModal" >
+          <a class="navbar-nav ml-auto" href="index.php" data-toggle="modal" data-target="#logoutModal" >
           <i class="large material-icons">exit_to_app</i>
             Logout
           </a>
@@ -115,27 +151,61 @@
         <!-- Begin Page Content -->
         <div class="container-fluid">
 
-          <!-- Page Heading -->
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-          </div>
-
           <!-- Content Row -->
-          <div class="row">
+         
 
-            <!-- Earnings (Monthly) Card Example -->
-            
-          
-          <!-- Content Row -->
+            <!-- Section: Team v.2 -->
+<section class="team-section text-center my-5">
 
-          
+  <!-- Section heading -->
+  <h2 class="h1-responsive font-weight-bold my-5">Our amazing team</h2>
+  <!-- Section description -->
+  
 
+  <!-- Grid row -->
+  <div class="row text-center">
+
+    <!-- Grid column -->
+    <div class="col-md-4 mb-md-0 mb-5">
+      <br>
+      <img class="col-md-4" src="img/nicky.jpg">
+      <h4 class="font-weight-bold dark-grey-text my-4">Nicky Dwi Rizki</h4>
+      <h6 class="text-uppercase grey-text mb-3"><strong>1700018090</strong></h6>
+      <h6 class="text-uppercase grey-text mb-3"><strong>Analyts</strong></h6>
       
-           
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-md-4 mb-md-0 mb-5" >
+      <img class="col-md-4" src="img/qeela.jpeg">
+      <h4 class="font-weight-bold dark-grey-text my-4">Rizqa Tsaqila Aulia Haq</h4>
+      <h6 class="text-uppercase grey-text mb-3"><strong>1700018114</strong></h6>
+      <h6 class="text-uppercase grey-text mb-3"><strong>Programmer</strong></h6>
+      
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-md-4">
+      <br>
+      <img class="col-md-4" src="img/sundari.jpg">
+      <h4 class="font-weight-bold dark-grey-text my-4">Sundari Fatikasary</h4>
+      <h6 class="text-uppercase grey-text mb-3"><strong>1700018065</strong></h6>
+      <h6 class="text-uppercase grey-text mb-3"><strong>Designer</strong></h6>
+      
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</section>
+<!-- Section: Team v.2 -->
 
 
             
-          </div>
+         
 
         </div>
         <!-- /.container-fluid -->
@@ -159,43 +229,7 @@
   </div>
   <!-- End of Page Wrapper -->
 
-  
 
-  <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
-
-  <!-- Page level plugins -->
-  <script src="vendor/chart.js/Chart.min.js"></script>
-
-  <!-- Page level custom scripts -->
-  <script src="js/demo/chart-area-demo.js"></script>
-  <script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
 
